d77ee9c4028c7c5ee37db2e900026837a1566206
Владимир Губин <gubinvs@gmail.com>
2021-03-18 21:30:08 +0500

    Добавьте файлы проекта.

---
+++ 09_HW_GubinVS/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _09_HW_GubinVS
+{
+    class Config
+    {
+
+        /// <summary>
+        ///  Путь к файлу с токеном телеграмм
+        /// </summary>
+        private static string path = @"C:\Users\gubinvs\Desktop\teltoken.txt";
+
+        /// <summary>
+        /// Токет чат бота телеграмм, считывается из файла
+        /// </summary>
+        public static string Token { get;} = File.ReadAllText(path, Encoding.UTF8);
+
+    
+    }
+}
---
+++ 09_HW_GubinVS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace _09_HW_GubinVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string token = Config.Token; // Токен телеграмм бота
+
+           
+
+            WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
