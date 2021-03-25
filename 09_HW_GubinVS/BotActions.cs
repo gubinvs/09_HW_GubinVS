b91c8697ec722ff29efedd23eb69e5c0951624a0
Владимир Губин <gubinvs@gmail.com>
2021-03-25 13:52:01 +0500

    Edit  BotActionsDownloadFoto

--- 09_HW_GubinVS/BotActions.cs
+++ 09_HW_GubinVS/BotActions.cs
@@ -66,8 +66,8 @@ namespace _09_HW_GubinVS
         }
 
         public static void DownloadFoto(GetUpdates getUpdates)
-        { 
-        
+        {
+            Console.WriteLine("Пришло сообщение содержащее фото");
         
         }
 
