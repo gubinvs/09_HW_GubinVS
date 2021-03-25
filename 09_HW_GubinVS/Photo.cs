c62eb09d496ef82767fc9ffab68e5c96f7211c36
Владимир Губин <gubinvs@gmail.com>
2021-03-25 12:55:44 +0500

    Add class Photo

---
+++ 09_HW_GubinVS/Photo.cs
@@ -0,0 +1,10 @@
+﻿
+public class Photo
+{
+    public string file_id { get; set; }
+    public string file_unique_id { get; set; }
+    public int file_size { get; set; }
+    public int width { get; set; }
+    public int height { get; set; }
+}
+
