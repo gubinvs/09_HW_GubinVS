7d62e57143d17ec668c812ec5e7d07df6e66ebad
Владимир Губин <gubinvs@gmail.com>
2021-03-25 14:33:52 +0500

    editing input parameters Programs

--- 09_HW_GubinVS/Program.cs
+++ 09_HW_GubinVS/Program.cs
@@ -27,19 +27,19 @@ namespace _09_HW_GubinVS
                 
                 GetUpdates gu = JsonSerializer.Deserialize<GetUpdates>(str);                                        // Дисериализация входящего сообщения в класс GetUpdates
 
-                if (gu.result.Length != 0)                                                                          // проверка не пустое ли сообщение
+                if (gu.result.Any(x => x.update_id != 0)) //gu.result.Length != 0                                                                    // проверка не пустое ли сообщение
                 {
                     update_id = gu.result[0].update_id + 1;                                                         // Прибавляем еденицу к текущему сообщению (отметили как прочитанное)
-                    
-                    if (gu.result[0].message.text != null)
+
+                    if (gu.result.Any(x => x.message.text != null))
                     {
                         BotActions.PrintMessage(gu.result[0].message.text, gu.result[0].message.from.first_name);   // если в сообщении есть текст => вывести его в консоль
                     }
-                    else if (gu.result[0].message.document.file_name != null)                                       // если есть в сообщении документ 
+                    else if (gu.result.Any(x => x.message.document != null))                                       // если есть в сообщении документ 
                     {
                         BotActions.DownloadFile(gu);                                                                // Скачивает файл на диск
                     }
-                    else if (gu.result[0].message.Photo.Length != 0)
+                    else if (gu.result.Any(x => x.message.Photo != null))
                     {
                         BotActions.DownloadFoto(gu);
                     }
