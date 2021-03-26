afca212b322762e632eb9398682d7cabe991fb74
Владимир Губин <gubinvs@gmail.com>
2021-03-26 11:51:30 +0500

    Add class Sticker

--- 09_HW_GubinVS/Message.cs
+++ 09_HW_GubinVS/Message.cs
@@ -1,12 +1,24 @@
 ﻿    public class Message
     {
         public int message_id { get; set; }
-        public From from { get; set; }
-        public Chat chat { get; set; }
-        public int date { get; set; }
-        public string text { get; set; }
-        public Document document { get; set; }
-        public Photo[] photo { get; set; } 
 
-    }
+        public From from { get; set; }
+
+        public Chat chat { get; set; }
+
+        public int date { get; set; }
+
+        public string text { get; set; }
+
+        public Document document { get; set; }
+
+        public Sticker sticker { get; set; }
+
+        public Photo[] photo { get; set; }
+
+
+
+
+
+}
 
---
+++ 09_HW_GubinVS/Sticker.cs
@@ -0,0 +1,12 @@
+﻿public class Sticker
+{
+    public int width { get; set; }
+    public int height { get; set; }
+    public string emoji { get; set; }
+    public string set_name { get; set; }
+    public bool is_animated { get; set; }
+    public Thumb thumb { get; set; }
+    public string file_id { get; set; }
+    public string file_unique_id { get; set; }
+    public int file_size { get; set; }
+}
